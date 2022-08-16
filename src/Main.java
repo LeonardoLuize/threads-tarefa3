4bf1ef889c45648a8069b031371edea7014d910b
leonardofalango <leo_falango@hotmail.com>
2022-08-16 20:27:46 -0300

    Iniciliza a classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
-    // O comentario
+        FilaVenda fila_venda = new FilaVenda(1);
+
+        Semaphore s1 = new Semaphore(1);
+        Semaphore s2 = new Semaphore(0);
+
+        int contador_vendas = 0;
+        Loja loja1 = new Loja("A", contador_vendas);
+
+        Fabricante fabricante = new Fabricante();
     }
 }
