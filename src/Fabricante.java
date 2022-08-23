23103c809f55f23060c8bb82c61c52de42e194cb
Leonardo Luize <leonardo.luize2@gmail.com>
2022-08-22 21:58:18 -0300

    Feat/classe fabricante (#7)
    * tentativa 2
    
    * criando a fabricante
    
    * fabricante versao 2
    
    * atualiza classe fabricante pra versao 2
    
    * fix: ajusta
    
    Co-authored-by: Beatriz Jardim <paulabeatrizlj@hotmail.com>
    Co-authored-by: Paula Beatriz Louback Jardim <paula.jardim@pucpr.edu.br>

--- src/Fabricante.java
+++ src/Fabricante.java
@@ -3,12 +3,14 @@ import java.util.concurrent.Semaphore;
 public class Fabricante extends Thread{
 
     private FilaVenda fila_vendas;
+    private FilaEntrega fila_entrega;
     private Semaphore mutex;
     private Semaphore itens;
     private Semaphore espacos;
 
-    public Fabricante(FilaVenda fila_vendas, Semaphore mutex, Semaphore itens, Semaphore espacos){
+    public Fabricante(FilaVenda fila_vendas, FilaEntrega fila_entrega, Semaphore mutex, Semaphore itens, Semaphore espacos){
         this.fila_vendas = fila_vendas;
+        this.fila_entrega = fila_entrega;
         this.mutex = mutex;
         this.itens = itens;
         this.espacos = espacos;
@@ -23,6 +25,7 @@ public class Fabricante extends Thread{
                 System.out.println();
                 Venda fabricado = fila_vendas.pop();
                 System.out.println("Fabricado: " + fabricado.getIdVenda());
+                Entrega ent = fila_entrega.append(ent);
 
                 mutex.release();
                 espacos.release();
