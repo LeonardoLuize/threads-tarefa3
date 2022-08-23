e9f227f5fa60e45ac2a2dad21b5a573d9b56817a
Paula Beatriz Louback Jardim <paula.jardim@pucpr.edu.br>
2022-08-22 21:15:47 -0300

    fabricante versao 2

--- src/Fabricante.java
+++ src/Fabricante.java
@@ -3,12 +3,14 @@ import java.util.concurrent.Semaphore;
 public class Fabricante extends Thread{
 
     private FilaVenda fila_vendas;
+    private FilaEntrega fila_entrega;
     private Semaphore mutex;
     private Semaphore itens;
     private Semaphore espacos;
 
-    public Fabricante(FilaVenda fila_vendas, Semaphore mutex, Semaphore itens, Semaphore espacos){
+    public Fabricante(FilaVenda fila_vendas, FilaEntrega fila_entrega, Semaphore mutex, Semaphore itens, Semaphore espacos){
         this.fila_vendas = fila_vendas;
+        this.fila_entrega = fila_entrega;
         this.mutex = mutex;
         this.itens = itens;
         this.espacos = espacos;
@@ -22,6 +24,7 @@ public class Fabricante extends Thread{
 
                 Venda fabricado = fila_vendas.pop();
                 System.out.println("Fabricado: " + fabricado.getIdVenda());
+                Entrega ent = fila_entrega.append();
 
                 mutex.release();
                 espacos.release();
